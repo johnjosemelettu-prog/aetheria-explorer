a7413de4be8a06d9a2cbd011f59850d3d71ad801
john jose <johnjosemelettu@gmail.com>
2026-04-09 20:46:08 +1000

    globalisation  step 2

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -7,20 +7,44 @@ import enTranslations from '../locales/en.json';
 import esTranslations from '../locales/es.json';
 import frTranslations from '../locales/fr.json';
 import jaTranslations from '../locales/ja.json';
+import deTranslations from '../locales/de.json';
+import mlTranslations from '../locales/ml.json';
+import arTranslations from '../locales/ar.json';
+import taTranslations from '../locales/ta.json';
+import hiTranslations from '../locales/hi.json';
+import msTranslations from '../locales/ms.json';
+import tlTranslations from '../locales/tl.json';
+import zhTranslations from '../locales/zh.json';
+import ptTranslations from '../locales/pt.json';
+import ruTranslations from '../locales/ru.json';
+import urTranslations from '../locales/ur.json';
+import idTranslations from '../locales/id.json';
+import pcmTranslations from '../locales/pcm.json';
+import trTranslations from '../locales/tr.json';
+import teTranslations from '../locales/te.json';
+import knTranslations from '../locales/kn.json';
 
 const resources = {
-  en: {
-    translation: enTranslations,
-  },
-  es: {
-    translation: esTranslations,
-  },
-  fr: {
-    translation: frTranslations,
-  },
-  ja: {
-    translation: jaTranslations,
-  }
+  en: { translation: enTranslations },
+  es: { translation: esTranslations },
+  fr: { translation: frTranslations },
+  ja: { translation: jaTranslations },
+  de: { translation: deTranslations },
+  ml: { translation: mlTranslations },
+  ar: { translation: arTranslations },
+  ta: { translation: taTranslations },
+  hi: { translation: hiTranslations },
+  ms: { translation: msTranslations },
+  tl: { translation: tlTranslations },
+  zh: { translation: zhTranslations },
+  pt: { translation: ptTranslations },
+  ru: { translation: ruTranslations },
+  ur: { translation: urTranslations },
+  id: { translation: idTranslations },
+  pcm: { translation: pcmTranslations },
+  tr: { translation: trTranslations },
+  te: { translation: teTranslations },
+  kn: { translation: knTranslations },
 };
 
 i18n
