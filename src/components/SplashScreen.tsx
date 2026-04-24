4bc3e3844d460ab3dcea1c0560e2a6f3fb8c092d
johnjosemelettu <johnjosemelettu@gmail.com>
2026-04-24 05:15:17 +0000

    splashscreen fixes

---
+++ src/components/SplashScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SplashScreen: React.FC = () => {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-white">Aetheria Explorer</h1>
+    </div>
+  );
+};
+
+export default SplashScreen;
